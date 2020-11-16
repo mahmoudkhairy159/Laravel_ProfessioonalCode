d95ca90df909e1b4e023be7c1a8dbef703bcff5f
unknown <mahmoudkhairy159@gmail.com>
2020-11-16 04:22:14 +0200

    Laravel task scheduler --> user expiration

---
+++ app/Console/Commands/Expiration.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Facade\Ignition\Support\Packagist\Package;
+use Illuminate\Console\Command;
+use App\User;
+
+class Expiration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:expire';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'expire users every hour automatically';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users=User::where('expire',0)->get();
+        foreach ($users as $user){
+            $user->update(['expire'=>1]);
+        }
+
+    }
+}
